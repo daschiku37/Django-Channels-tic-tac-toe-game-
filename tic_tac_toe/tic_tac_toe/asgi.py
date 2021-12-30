e9ba7eccf522e895d0b57caa54f5e15659f60440
daschiku37 <daschiku37@gmail.com>
2021-12-30 23:47:05 +0530

    ASGI CONFIG

--- tic_tac_toe/tic_tac_toe/asgi.py
+++ tic_tac_toe/tic_tac_toe/asgi.py
@@ -9,13 +9,20 @@ https://docs.djangoproject.com/en/3.2/howto/deployment/asgi/
 
 import os
 
-from channels.routing import ProtocolTypeRouter
+from channels.routing import ProtocolTypeRouter,URLRouter
 from django.core.asgi import get_asgi_application
+from channels.auth import AuthMiddlewareStack
+import game.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tic_tac_toe.settings')
 
 # application = get_asgi_application()
 application = ProtocolTypeRouter({
     "http":get_asgi_application(),
+    "websocket":AuthMiddlewareStack(
+        URLRouter(
+            game.routing.websocket_urlpatterns
+        )
+    ),
 })
 
